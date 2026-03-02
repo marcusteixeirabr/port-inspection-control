6d4254886ac52472545ba0837a7ad89483f8b3d4
marcusteixeirabr <marcusteixeirabr@gmail.com>
2026-03-02 22:19:55 +0000

    Initial spring boot structure

---
+++ src/main/java/br/com/gvi/portinspection/PortinspectionApplication.java
@@ -0,0 +1,13 @@
+package br.com.gvi.portinspection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortinspectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortinspectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gvi/portinspection/PortinspectionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gvi.portinspection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortinspectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
